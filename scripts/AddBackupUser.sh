91ad217d3890fd5c34ac9fea50711c57e0bb781b
othermusketeer <djp_bravofleet@hotmail.com>
2014-06-08 00:15:48 -0400

    Added checks to a couple scripts.
    Add several checks to the scripts ensure sufficient permissions and to
    carryout only the actions needed. Added a few checks to make sure
    commands executed successfully. More such checks need to be added
    in the future.
    
    Changes:
     - scripts/AddBackupUser.sh
    	2014/6/7	Added conditionals to carry out actions only if
    		needed. Added root permision check.
    
     - scripts/SetupBackupServer
    	2014/6/7	Added conditionals to carry out actions only if
    		needed. Added root permision check.

--- scripts/AddBackupUser.sh
+++ scripts/AddBackupUser.sh
@@ -3,6 +3,11 @@
 # Author:   D'Artagnan Palmer
 # License:  Copyright (c) 2014, The MIT License (MIT)
 # Summary:  Add a new user, and configure to allow receiving backups
+#
+#
+#
+#	2014/6/7	Added conditionals to carry out actions only if needed. Added root permision check.
+#
 
 BU_USER="$1"
 KEY_COMMENT="$2"
@@ -11,16 +16,112 @@ KEY_PASSWORD="$3"
 BU_BASE=/var/backups/external/
 BU_GROUP=ebackups
 BU_USER_PREFIX="bu_"
+ROOTUID="0"
 
-# -d homedir, -g groupid, -N (no create group), -M (no create homedir)
-useradd ${BU_USER_PREFIX}${BU_USER} -d /${BU_USER} -g ${BU_GROUP} -N -M
+# Check/ensure user has root privileges.
+# From StackOverflow answer by mhoareau, http://stackoverflow.com/a/6362626/3453155
+if [ "$(id -u)" -ne "$ROOTUID" ] ; then
+    echo "This script must be executed with root privileges."
+    exit 1
+fi
 
-mkdir ${BU_BASE}${BU_USER}
-chown ${BU_USER_PREFIX}${BU_USER}:${BU_GROUP} ${BU_BASE}${BU_USER}
-chmod o-rwx ${BU_BASE}${BU_USER}
-mkdir "${BU_BASE}{$BU_USER}/.ssh"
-chown ${BU_USER_PREFIX}${BU_USER} "${BU_BASE}{$BU_USER}/.ssh"
-chmod 600 "${BU_BASE}{$BU_USER}/.ssh"
-ssh-keygen -t rsa -C "$KEY_COMMENT" -N "${KEY_PASSWORD}" -f "${BU_BASE}{$BU_USER}/.ssh/backup_rsa"
-chown -R ${BU_USER_PREFIX}${BU_USER} "${BU_BASE}{$BU_USER}/.ssh"
-chmod -R 600 "${BU_BASE}{$BU_USER}/.ssh"
+getent group ${BU_GROUP} >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo "ERROR: The specified GID for backups, '${BU_GROUP}', does not exist!"
+	echo "ERROR:     You need to run SetupBackServer.sh first. Aborting!!!"
+	exit 2
+fi
+
+if [ ! -r "${BU_BASE}" ]; then
+	echo "ERROR: Unable to read base directory used for backups, '${BU_BASE}'!"
+	echo "ERROR:     You might need to run SetupBackServer.sh first. Aborting!!!"
+	exit 3
+fi
+
+id -u  ${BU_USER_PREFIX}${BU_USER} >/dev/null 2>&1
+if [ ! $? -eq 0 ]; then
+	echo "Adding user '${BU_USER_PREFIX}${BU_USER}'..."
+	# -d homedir, -g groupid, -N (no create group), -M (no create homedir)
+	useradd ${BU_USER_PREFIX}${BU_USER} -d /${BU_USER} -g ${BU_GROUP} -N -M
+else
+	echo "ERROR: User '${BU_USER_PREFIX}${BU_USER}' already exists! Aborting!!!"
+	exit 4
+fi
+
+# If backup directory exists, make sure its suitable. If it doesn't, make it.
+if [ -d "${BU_BASE}${BU_USER}" ]; then
+	echo "WARNING: Backup directory '${BU_BASE}${BU_USER}' exists! Checking suitability..."
+	sleep 2
+	if [ -n "$(find "${BU_BASE}${BU_USER}" -user "${BU_USER_PREFIX}${BU_USER}" -print -prune -o -prune)" ]; then
+		echo "    The user ownership is right. Still checking..."
+		if [ -n "$(find "${BU_BASE}${BU_USER}" -group "${BU_GROUP}" -print -prune -o -prune)" ]; then
+			echo "    The group ownership is also right. Proceeding after a delay..."
+			sleep 2
+		else
+			echo "    ERROR: User ownership is wrong! Aborting!!!"
+			exit 5
+		fi
+	else
+		echo "    ERROR: Group ownership is wrong! Aborting!!!"
+		exit 6
+	fi
+else
+	echo "Creating backup directory '${BU_BASE}${BU_USER}'..."
+	mkdir "${BU_BASE}${BU_USER}"
+	chown ${BU_USER_PREFIX}${BU_USER}:${BU_GROUP} ${BU_BASE}${BU_USER}
+	chmod o-rwx ${BU_BASE}${BU_USER}
+fi
+
+if [ ! -d "${BU_BASE}{$BU_USER}/.ssh" ]; then
+	echo "SSH credentials directory doesn't exist. Creating..."
+	mkdir "${BU_BASE}{$BU_USER}/.ssh"
+fi
+
+if [ -d "${BU_BASE}{$BU_USER}/.ssh" ]; then
+	echo "Enforcing ownership and permissions of SSH credentials directory..."
+	chown ${BU_USER_PREFIX}${BU_USER} "${BU_BASE}{$BU_USER}/.ssh"
+	chmod 600 "${BU_BASE}{$BU_USER}/.ssh"
+else
+	echo "ERROR: SSH credentials folder doesn't exist, even after trying to create it! Aborting!!!"
+	exit 7
+fi
+
+if [ ! -w "${BU_BASE}{$BU_USER}/.ssh" ]; then
+	echo "WARNING: No write access to credentials directory. Temporarily fixing..."
+	chown root "${BU_BASE}{$BU_USER}/.ssh"
+	chmod 600 "${BU_BASE}{$BU_USER}/.ssh"
+fi
+
+if [ -w "${BU_BASE}{$BU_USER}/.ssh" ]; then
+	if [ -z "${KEY_PASSWORD}" ]; then
+		echo "WARNING: No password specified (argument #3 to this script) for SSH key!"
+		echo "    Key will not be password protected! Proceeding after delay..."
+		sleep 2
+	fi
+	echo "Generating RSA key..."
+	ssh-keygen -t rsa -C "$KEY_COMMENT" -N "${KEY_PASSWORD}" -f "${BU_BASE}{$BU_USER}/.ssh/backup_rsa"
+else
+	echo "ERROR: Unable to write to SSH credentials directory! Aborting!!!"
+	exit 8
+fi
+
+if [ -r "${BU_BASE}{$BU_USER}/.ssh/backup_rsa" ]; then
+	echo "Reading RSA fingerprint..."
+	KEY_FINGERPRINT=$(ssh-keygen -l -f "${BU_BASE}{$BU_USER}/.ssh/backup_rsa")
+	echo "Enforcing ownerships and permissions on SSH credentials directory..."
+	chmod -R 600 "${BU_BASE}{$BU_USER}/.ssh"
+	chown -R ${BU_USER_PREFIX}${BU_USER} "${BU_BASE}{$BU_USER}/.ssh"
+else
+	echo "ERROR: Something went wrong generating the RSA key! Aborting!!!"
+	exit 9
+fi
+
+echo "Script finished for user '${BU_USER}:"
+echo "    Login:  ${BU_USER_PREFIX}${BU_USER}        Group:  ${BU_GROUP}"
+echo "    Directory:  ${BU_BASE}{$BU_USER}"
+echo "    RSA fingerprint: ${KEY_FINGERPRINT}"
+echo ""
+
+# TODO: copy or email public key to user. For security reasons, maybe not.
+
+echo "Script completed for user ${BU_USER} (${BU_BASE}{$BU_USER})."
--- scripts/SetupBackupServer.sh
+++ scripts/SetupBackupServer.sh
@@ -1,27 +1,61 @@
 #!/bin/sh
-#
+
 # Author:   D'Artagnan Palmer
 # License:  Copyright (c) 2014, The MIT License (MIT)
 # Summary:  Setup server to receive connections from backup
 
-#  You should only have to run this once
+#	You should only have to run this once
+#
+#	2014/6/7	Added conditionals to carry out actions only if needed. Added root permision check.
+#
 
 # please end it with "/"
 BU_BASE=/var/backups/external/
 BU_GROUP=ebackups
 BU_USER_PREFIX="bu_"
+ROOTUID="0"
 
-mkdir "${BU_BASE}"
-chown root:root "${BU_BASE}"
-chmod 755 "$BU_BASE"
+# Check/ensure user has root privileges.
+# From StackOverflow answer by mhoareau, http://stackoverflow.com/a/6362626/3453155
+if [ "$(id -u)" -ne "$ROOTUID" ] ; then
+    echo "This script must be executed with root privileges."
+    exit 1
+fi
 
-addgroup ${BU_GROUP}
 
-sed -i -e 's/Subsystem sftp .*$/Subsystem sftp internal-sftp/' /etc/ssh/sshd_config
+if [ ! -r "${BU_BASE}" ]; then
+	echo "Adding base directory '${BU_BASE}'..."
+	mkdir "${BU_BASE}"
+	chown root:root "${BU_BASE}"
+	chmod 755 "$BU_BASE"
+else
+	echo "Base directory '${BU_BASE}' already exists..."
+fi
+
+getent group ${BU_GROUP} >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo "Add group '${BU_GROUP}'..."
+	addgroup ${BU_GROUP}
+else
+	echo "Group '${BU_GROUP}' already exists..."
+fi
+
+grep '^Subsystem sftp internal-sftp' /etc/ssh/sshd_config
+if [ $? -ne 0 ]; then
+	echo "Modifying sshd_config to support sftp backups..."
+	cp -a /etc/ssh/sshd_config /etc/ssh/sshd_conf.orig.bu
+	sed -i -e 's/Subsystem sftp .*$/Subsystem sftp internal-sftp/' /etc/ssh/sshd_config
+	echo "Match group ${BU_GROUP}" >> /etc/ssh/sshd_config
+	echo "ChrootDirectory ${BU_BASE}" >> /etc/ssh/sshd_config
+	echo "X11Forwarding no" >> /etc/ssh/sshd_config
+	echo "AllowTcpForwarding no" >> /etc/ssh/sshd_config
+	echo "ForceCommand internal-sftp" >> /etc/ssh/sshd_config
+else
+	echo "Modifications to sshd_config seem to not be needed..."
+fi
+echo ""
+
+echo "Group '${BU_GROUP}' will back up to directory '${BU_BASE}'."
 
 echo ""
-echo "Match group ebackups" >> /etc/ssh/sshd_config
-echo "ChrootDirectory ${BU_BASE}" >> /etc/ssh/sshd_config
-echo "X11Forwarding no" >> /etc/ssh/sshd_config
-echo "AllowTcpForwarding no" >> /etc/ssh/sshd_config
-echo "ForceCommand internal-sftp" >> /etc/ssh/sshd_config
+echo "SetupBackupServer.sh is now done."
